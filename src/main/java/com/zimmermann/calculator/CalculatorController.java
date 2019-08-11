10fb6dc938213a4caaa633b0cdbbf212abe950a1
Igor Zimmermann <igor.sonstnix@gmail.com>
2019-08-11 23:04:35 +0200

    # master: SonarQube refactoring 3.0

--- src/main/java/com/zimmermann/calculator/CalculatorController.java
+++ src/main/java/com/zimmermann/calculator/CalculatorController.java
@@ -1,7 +1,7 @@
 package com.zimmermann.calculator;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,10 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class CalculatorController {
+
   @Autowired
   private Calculator calculator;
-  @RequestMapping("/sum")
-  String sum(@RequestParam("a") Integer a,
+
+  @GetMapping("/sum")
+  public String sum(@RequestParam("a") Integer a,
       @RequestParam("b") Integer b) {
     return String.valueOf(calculator.sum(a, b));
   }
