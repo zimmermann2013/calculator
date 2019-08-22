a6f5ada84fe923e86d7ad3654f8306b712698c00
Igor Zimmermann <igor.sonstnix@gmail.com>
2019-08-22 14:42:01 +0200

    # feature: add JavaDoc kommentar 1.0

--- src/test/java/com/zimmermann/calculator/CalculatorApplicationTests.java
+++ src/test/java/com/zimmermann/calculator/CalculatorApplicationTests.java
@@ -5,10 +5,16 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+/**
+ * Hiet testen wir die gesamt Application
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CalculatorApplicationTests {
 
+  /**
+   * Hier wird der ContextLoader getestet
+   */
   @Test
   public void contextLoads() {
   }
