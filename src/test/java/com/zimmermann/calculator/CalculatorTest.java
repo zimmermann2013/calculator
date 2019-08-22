ef8bd63bbe54e4c1fca3aea557244b4ad9d7f6fe
Igor Zimmermann <igor.sonstnix@gmail.com>
2019-08-22 14:38:39 +0200

    # feature: add JavaDoc kommentar

--- src/test/java/com/zimmermann/calculator/CalculatorTest.java
+++ src/test/java/com/zimmermann/calculator/CalculatorTest.java
@@ -4,10 +4,16 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+/**
+ * Hier testen wir alle Mehtoden
+ */
 public class CalculatorTest {
 
   private Calculator calculator = new Calculator();
 
+  /**
+   * Hier testen wir die Sum-Methode
+   */
   @Test
   public void testSum() {
     assertEquals(5, calculator.sum(2, 3));
